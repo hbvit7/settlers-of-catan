990db002e88901f2dc921379dc5c6acc67a289fd
Jaden Carver <jaden.carver@gmail.com>
2015-02-25 16:59:51 -0500

    ActiveModel to_partial_path

--- settler/app/models/country.rb
+++ settler/app/models/country.rb
@@ -1,19 +1,17 @@
 class Country < ActiveRecord::Base
 	belongs_to :player
 
-	after_create :set_population, :set_flag
+	before_create :set_population, :set_flag
 
 	private
 
 	def set_flag
 		flags = Dir[Rails.root.join('app/assets/images/flags/*.png')]
 		self.flag = flags.sample
-		self.save!
 	end
 
 	def set_population
 		self.population = rand(0..100)
-		self.save!
 	end
 	
 end
