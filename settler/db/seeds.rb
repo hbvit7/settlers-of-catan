3bf86804c15259f28b0aafdb27e027a465b5f125
Dan Snider <dansnider@gmail.com>
2015-02-25 16:05:51 -0500

    seeds.rb filled out

--- settler/db/seeds.rb
+++ settler/db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Country.destroy_all
+Player.destroy_all
+
+flags = Dir[Rails.root.join('app/assets/images/flags/*.png')]
+
+anya = Player.create!({
+	name: 'Anya Glowa-Kollisch',
+	title: 'Empress Regnant',
+	email: 'anya@email.com',
+	password: 'anyapassword'
+	})
+
+izabella = Player.create!({
+	name: 'Izabella Stern',
+	title: 'Countess',
+	email: 'izabella@email.com',
+	password: 'izabellapassword'
+	})
+
+robin = Player.create!({
+	name: 'Robin Mesina',
+	title: 'Czar',
+	email: 'robin@email.com',
+	password: 'robinpassword'
+	})
+
+dan = Player.create!({
+	name: 'Dan Snider',
+	title: 'Lord',
+	email: 'dan@email.com',
+	password: 'danpassword'
+	})
+
+Country.create!({
+	name: "Christmas Island",
+	player_id: dan.id,
+	flag: flags.sample,
+	motto: "We got a ton of crabs here.",
+	employment: 0.88,
+	tax_rate: 0.06,
+	wealth: 2000000
+	})
+
+Country.create!({
+	name: "Skull Island",
+	player_id: robin.id,
+	flag: flags.sample,
+	motto: "Enter if you dare! (The island looks like a skull).",
+	employment: 0.89,
+	tax_rate: 0.04,
+	wealth: 3000000
+	})
+
+Country.create!({
+	name: "Anyastan",
+	player_id: anya.id,
+	flag: flags.sample,
+	motto: "Welcome to Anyastan, land of splendor.",
+	employment: 0.9,
+	tax_rate: 0.03,
+	wealth: 3500000
+	})
+
+Country.create!({
+	name: "Narnia",
+	player_id: izabella.id,
+	flag: flags.sample,
+	motto: "Always Winter but never Christmas.",
+	employment: 0.88,
+	tax_rate: 0.04,
+	wealth: 3200000
+	})
+
