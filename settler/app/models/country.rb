38bdd2cac4885c0373e8c41c173456e96aed39a5
Dan Snider <dansnider@gmail.com>
2015-02-25 16:02:31 -0500

    added flags and proper random population methods

--- settler/app/models/country.rb
+++ settler/app/models/country.rb
@@ -1,8 +1,19 @@
 class Country < ActiveRecord::Base
 	belongs_to :player
 
-	after_create do
+	after_create :set_population, :set_flag
+
+	private
+
+	def set_flag
+		flags = Dir[Rails.root.join('app/assets/images/flags/*.png')]
+		self.flag = flags.sample
+		self.save!
+	end
+
+	def set_population
 		self.population = rand(0..100)
+		self.save!
 	end
 	
 end
