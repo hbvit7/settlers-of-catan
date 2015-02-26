ef2ff4b6f6a29a3ea9a2ef3bc86965b82c591dea
Anya Glowa-Kollisch <anyagk@gmail.com>
2015-02-25 19:09:06 -0500

    routes sessions players

--- settler/app/controllers/application_controller.rb
+++ settler/app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include PlayersHelper
   protect_from_forgery with: :exception
+
+  def login_required
+    unless current_player
+      render :permission_denied, status: :permission_denied
+    end
+  end
+
+
 end
